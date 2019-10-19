2e9a036eda543308c9a178765d9efa2b5b3dd19a
Aresky Berkane <areskyberkane@gmail.com>
2019-10-19 14:15:20 +0100

    completed some exersices

--- pet_shop.rb
+++ pet_shop.rb
@@ -22,9 +22,22 @@ def stock_count(shop)
     return shop[:pets].length()
 end
 
-# here I return the total number of the same breed of, but I deleted the .count method on the test
+
 def pets_by_breed(shop,breed)
-  total_num = 0
-  shop[:pets].each{|x| total_num += 1 if x[:breed] == breed}
+  total_num = []
+  shop[:pets].each{|x| total_num.push(x[:breed]) if x[:breed] == breed}
   return total_num
+end
+
+def find_pet_by_name(shop,name)
+  for pet in shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name (shop,name)
+  shop[:pets].each{|x| x == nil if x[:name] == name}
 end
